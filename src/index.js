8d597e98ef2529df20e33b4bd4a1608df8e992e3
Suellen Castro <castrosuellenx@gmail.com>
2021-09-14 14:27:04 -0300

    logic

--- src/index.js
+++ src/index.js
@@ -39,7 +39,30 @@ export default class App extends Component {
     this.setState({...initialState});
   };
 
-  setOperation = operation => {};
+  setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({operation, current: 1, clearDisplay: true});
+    } else {
+      const equals = operation === '=';
+      const values = [...this.state.values];
+
+      try {
+        // eslint-disable-next-line no-eval
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]} `);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: true,
+        values,
+      });
+    }
+  };
 
   render() {
     return (
